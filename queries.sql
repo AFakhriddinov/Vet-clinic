1e640f53c293c4c582e6d1d58d92d0f227c5d0f6
Abdulaziz Fakhriddinov <mr_abdulaziz777@list.ru>
2023-06-20 18:03:48 +1000

    Write queries

--- queries.sql
+++ queries.sql
@@ -6,3 +6,35 @@ select name, escape_attempts from animals where weight_kg > 10.5;
 select * from animals where neutered = true;
 select * from animals where  name <> 'Gabumon';
 select * from animals where weight_kg between 10.4 and 17.3;
+
+begin transaction;
+update animals set species = 'unspecified';
+select * from animals;
+rollback;
+select * from animals;
+update animals set species = 'digimon' where name like '%mon';
+update animals set species = 'pokemon' where species is null;
+select * from animals;
+commit;
+select * from animals;
+
+begin transaction;
+delete from animals;
+select * from animals;
+rollback;
+select * from animals;
+
+begin transaction;
+delete from animals where date_of_birth > '2022-01-01';
+SAVEPOINT del_savepoint;
+update animals set weight_kg = weight_kg * -1;
+rollback to SAVEPOINT del_savepoint;
+update animals set weight_kg = weight_kg * -1 where weight_kg < 0;
+commit;
+
+select count(*) from animals;
+select count(*) from animals where escape_attempts = 0;
+select avg(weight_kg) from animals;
+select neutered, max(escape_attempts) from animals  group by neutered;
+select species, min(weight_kg), max(weight_kg) from animals group by species;
+select species, avg(escape_attempts) from animals where date_of_birth between '1990-01-01' and '2000-12-31' group by species;
